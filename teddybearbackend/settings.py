64a7422ad0a5a3368b6e58a8536275bc99d5dbfe
Tri Tue Mai <HacTinhThan@gmail.com>
2022-12-06 23:51:48 +0700

    'caipackit1'

--- teddybearbackend/settings.py
+++ teddybearbackend/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TeddyBear.apps.TeddybearConfig',
+    'CustomUser.apps.CustomuserConfig',
+    'StoreManage.apps.StoremanageConfig',
+    'BlogPost.apps.BlogpostConfig',
 ]
 
 MIDDLEWARE = [
