36d738a482150bcff0698b2ba19808eac2941eb2
Tri Tue Mai <HacTinhThan@gmail.com>
2022-12-07 00:34:27 +0700

    'changecode01'

--- BlogPost/views.py
+++ BlogPost/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets, status, permissions, generics
+
+from .models import *
+from .serializers import *
+
+
+class PageSize10Pagiration(PageNumberPagination):
+    page_size = 10
+
+
+class PostViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAPIView):
+    queryset = Post.objects.filter(active=True)
+    queryset = queryset.order_by('-create_date')
+    serializer_class = PostSerializer
+    pagination_class = PageSize10Pagiration
 
 # Create your views here.
--- teddybearbackend/urls.py
+++ teddybearbackend/urls.py
@@ -14,8 +14,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Origin Movie API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="HacTinhThan@gmail.com"),
+        license=openapi.License(name="Mai Trí Tuệ @2022"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('TeddyBear.urls')),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('user/', include('CustomUser.urls')),
+    path('blog/', include('BlogPost.urls')),
+    path('manage/', include('StoreManage.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
 ]
